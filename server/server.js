06c279ef508cb0dcb735808e9a054fd842fb1424
Payal Yampure <payalyampure86@gmail.com>
2026-08-07 11:21:29 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -12,9 +12,12 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: [
+    "http://localhost:5173",
+    "https://event-buddy-green.vercel.app"
+  ],
+  credentials: true
 }));
-app.use(express.json());
 
 
 // Routes
